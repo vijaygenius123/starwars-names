9d53489e9b2b93705bc1472adc2a57e34f6f734f
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-22 22:19:20 +0530

    Added Test Cases

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,31 @@ var expect = require('chai').expect
 var starWarsNames = require('./index')
 
 describe('starwars-names', function () {
-    it('should work', function () {
-        expect(true).to.be.true
+
+    describe('all', () => {
+        it('Should be an array of strings', () => {
+            expect(starWarsNames.all).to.satisfy(isArrayOfStrings)
+
+            function isArrayOfStrings(array) {
+                return array.every(function (item) {
+                    return typeof item === 'string'
+                })
+            }
+        })
+
+        it('Should contain `Luke Skywalker`', () => {
+            expect(starWarsNames.all).to.include('Luke Skywalker')
+
+        })
     })
+
+    describe('random', () => {
+
+        it('Should return a random item from  starWarsNames.all ', () => {
+            var randomItem = starWarsNames.random()
+            expect(starWarsNames.all).to.include(randomItem)
+
+        })
+    })
+
 })
